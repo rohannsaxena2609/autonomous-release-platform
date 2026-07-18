ac3be5175b3b71e44cdaecf4d49c71b0e4b25af0
rohan <rohan_saxena26@outlook.com>
2026-07-18 18:55:20 +0530

    Release version 2.1 rollback test

--- src/main/java/com/rohan/autonomous_release_platform/controller/VersionController.java
+++ src/main/java/com/rohan/autonomous_release_platform/controller/VersionController.java
@@ -16,8 +16,8 @@ public class VersionController {
         Map<String, Object> response = new LinkedHashMap<>();
 
         response.put("application", "Autonomous Release Orchestration Platform");
-        response.put("version", "2.0.0-canary");
-        response.put("environment", "Canary");        
+        response.put("version", "2.1.0-rollback-test");
+        response.put("environment", "Rollback Test");     
         response.put("javaVersion", System.getProperty("java.version"));
         response.put("serverTime", LocalDateTime.now());
 
